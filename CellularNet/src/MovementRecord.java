d6b8ad8106d8674d1298b399002ba423d632640a
raf1906 <raf1906@raf1906-A300.(none)>
2014-05-12 14:19:38 +0200

    Modified group counting algorithm- vector instead of single integer. Allows to keep "movements records" for 5 quantums of time instead of incrementing/ decrementing int

--- CellularNet/src/MovementRecord.java
+++ CellularNet/src/MovementRecord.java
@@ -1,13 +1,18 @@
+import java.util.Vector;
+
 // Movement record class in monitor
 public class MovementRecord {
 	public String cellFrom; //from which cell was the movement
 	public String cellTo; //destination cell of movement
-	public int howManyMoved; //how many people moved so far
+	public Vector <Integer> movementQuantums; //how many people moved so far
 	public MovementRecord(String from, String to)
 	{
 		cellFrom=from;
 		cellTo=to;
-		howManyMoved=1;
+		movementQuantums=new Vector<Integer>();
+		movementQuantums.add(5);
 	}
+	
+
 	
 }
